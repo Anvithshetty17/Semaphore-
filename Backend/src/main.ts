370f53ceb5c83ba2b4db6d986e809bed8a8ecdbf
Maneesh Kumar <nnm24mc077@nmamit.in>
2025-08-30 13:24:33 +0000

    fixed cors

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://semaphore-olive.vercel.app',]
+    origin: ['http://localhost:3000', 'https://semaphore-olive.vercel.app','https://fantastic-tribble-7v7g94gprg7phxvjv-3000.app.github.dev']
   });
   await app.listen(3001);
 }
