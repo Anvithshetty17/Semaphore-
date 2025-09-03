6af193224a9ca708676badc7e0ecae73bc9b035e
Shreyas973 <shreyas.s29032004@gmail.com>
2025-09-03 18:41:24 +0530

    "registration ui"

--- Frontend/src/app/request-password-change/page.jsx
+++ Frontend/src/app/request-password-change/page.jsx
@@ -29,7 +29,7 @@ const RequestPasswordChange = () => {
         <>
          <Image src="/images/change-password-bg.jpg" alt="Background" fill className="absolute z-0 opacity-50 object-cover" priority/> 
             <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900  ">
-                <div className="flex flex-col items-center w-full max-w-sm mx-auto bg-white p-8 rounded-lg shadow-lg relative">
+                <div className="flex flex-col items-center w-full max-w-sm mx-auto bg-gray-500 p-8 rounded-lg shadow-lg relative">
                     <h2 className="text-2xl font-bold text-center mb-8 font-dosisBold text-black">Request Password Change</h2>
                     <p className="text-black text-center">Click the below button to recieve a link to change the password for your account !</p>
                     <div className="flex justify-center w-full font-dosisMedium mt-10">
