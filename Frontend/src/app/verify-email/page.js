18a30d9253574e23f78621424e7d4c9ff5eef027
Anup Nayak <115527833+anupnayak25@users.noreply.github.com>
2025-09-07 06:30:24 +0000

    verify email fixes..

--- Frontend/src/app/verify-email/page.js
+++ Frontend/src/app/verify-email/page.js
@@ -9,9 +9,11 @@ const VerifyEmail = () => {
     return (
         <>
             <Suspense fallback={<Loading />} >
-            <Image src="/images/change-password-bg.jpg" alt="Background" fill className="absolute z-0 opacity-10 object-cover" priority/>
-                <div className="flex flex-col w-screen h-screen overflow-hidden bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900  items-center justify-center">
-                    <VerifyEmailComponent />
+                <div className="relative flex flex-col w-screen h-screen overflow-hidden bg-gradient-to-r from-slate-900 via-purple-900 to-slate-900 items-center justify-center">
+                    <Image src="/images/change-password-bg.jpg" alt="Background" fill className="absolute inset-0 z-0 opacity-10 object-cover" priority/>
+                    <div className="relative z-10">
+                        <VerifyEmailComponent />
+                    </div>
                 </div>
             </Suspense>
         </>
