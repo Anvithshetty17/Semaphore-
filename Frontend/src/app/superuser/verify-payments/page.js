7d825cad0655d25c6933824a18d51cf741fe16cc
Shreyas973 <shreyas.s29032004@gmail.com>
2025-09-08 12:03:04 +0530

    "super user payment chnages"

--- Frontend/src/app/superuser/verify-payments/page.js
+++ Frontend/src/app/superuser/verify-payments/page.js
@@ -96,7 +96,8 @@ const VerifyPaymentsPage = () => {
                             type="button"
                             onClick={() => {
                                 setSelectPaymentId(null)
-                                setShowRejectPopup(false)
+                                // setShowRejectPopup(false)
+                                setShowAcceptPopup(false) // <-- Fix here
                             }}
                         >
                             Cancel
@@ -106,7 +107,7 @@ const VerifyPaymentsPage = () => {
             </form>}
             {showAcceptPopup === true && <div className="absolute top-32 right-10 bg-white p-4 border z-[999] w-[25vw] rounded-lg flex flex-col items-center space-y-6">
                 <p className="text-[20px] font-dosisBold text-green-700">Accept Payment</p>
-                <p> Are you sure you want to accept this payment  </p>
+                <p className="text-black"> Are you sure you want to accept this payment  </p>
                 <div className="flex flex-row space-x-2 w-full">
                     <div className="flex justify-center w-full font-dosisMedium">
                         <button
@@ -123,7 +124,8 @@ const VerifyPaymentsPage = () => {
                             type="button"
                             onClick={() => {
                                 setSelectPaymentId(null)
-                                setShowRejectPopup(false)
+                                // setShowRejectPopup(false)
+                                setShowAcceptPopup(false)
                             }}
                         >
                             Cancel
