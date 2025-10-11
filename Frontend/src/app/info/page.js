b3eb610b92f2ef1f16f0512c442582e042117251
Anup Nayak <115527833+anupnayak25@users.noreply.github.com>
2025-10-11 06:02:37 +0000

    deive link cjanged

--- Frontend/src/app/info/page.js
+++ Frontend/src/app/info/page.js
@@ -172,7 +172,7 @@ const TeamPage = () => {
         <div className="sticky top-0 z-30 bg-gradient-to-r from-purple-900/95 via-pink-900/95 to-purple-900/95 animate-gradient backdrop-blur-xl border-b border-purple-500/30">
           <div className="container mx-auto px-4 py-3">
             <div 
-              onClick={() => window.open('https://drive.google.com/drive/folders/1gqKQ0R0eZS5eIvdHEvH-DL0rj2UE82Ob', '_blank')
+              onClick={() => window.open('https://drive.google.com/drive/folders/1YZ_aQNooqS9XG6APu7nW-rWjz5Czrqmz?usp=drive_link', '_blank')
                 //toast.info("Gallery access will be available soon!")
                 }
               className="group flex items-center justify-center gap-3 cursor-pointer hover-scale transition-all"
